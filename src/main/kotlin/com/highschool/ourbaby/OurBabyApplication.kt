ca31e9413779c0f0e2c486c05b493178303c5c06
nopro <rjwharry2003@gmail.com>
2024-03-24 15:03:26 +0900

    init mommy project

---
+++ src/main/kotlin/com/highschool/ourbaby/OurBabyApplication.kt
@@ -0,0 +1,11 @@
+package com.highschool.ourbaby
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OurBabyApplication
+
+fun main(args: Array<String>) {
+	runApplication<OurBabyApplication>(*args)
+}
