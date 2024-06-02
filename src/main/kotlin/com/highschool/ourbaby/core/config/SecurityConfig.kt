f78748ed4059cdaf7e698f3c72d87a8a4c955e8b
joel <beanybigggy@gmail.com>
2024-06-02 13:12:38 +0900

    feat : security 설정

--- src/main/kotlin/com/highschool/ourbaby/core/config/SecurityConfig.kt
+++ src/main/kotlin/com/highschool/ourbaby/core/config/SecurityConfig.kt
@@ -2,10 +2,7 @@ package com.highschool.ourbaby.core.config
 
 import com.highschool.ourbaby.core.filter.OurBabyJwtFilter
 import com.highschool.ourbaby.member.persistence.repository.MemberRepository
-import com.highschool.ourbaby.oauth2.service.CustomOAuth2UserService
 import com.highschool.ourbaby.member.service.JwtService
-import com.highschool.ourbaby.oauth2.service.handler.OAuth2LoginFailureHandler
-import com.highschool.ourbaby.oauth2.service.handler.OAuth2LoginSuccessHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -18,12 +15,6 @@ import org.springframework.security.web.authentication.logout.LogoutFilter
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val successHandler: OAuth2LoginSuccessHandler,
-
-    private val failureHandler: OAuth2LoginFailureHandler,
-
-    private val customOAuth2UserService: CustomOAuth2UserService,
-
     private val jwtService: JwtService,
 
     private val memberRepository: MemberRepository,
@@ -37,16 +28,11 @@ class SecurityConfig(
             .headers { it.frameOptions { it.disable() } }
             .sessionManagement { it.sessionCreationPolicy(STATELESS) }
             .authorizeHttpRequests {
-                it.requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico").permitAll()
+                it.requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll()
                 it.requestMatchers("/").permitAll()
                 it.anyRequest().authenticated()
             }
             .addFilterAfter(ourBabyJwtFilter(), LogoutFilter::class.java)
-
-        // 원래 스프링 시큐리티 필터 순서가 LogoutFilter 이후에 로그인 필터 동작
-        // 따라서, LogoutFilter 이후에 우리가 만든 필터 동작하도록 설정
-        // 순서 : LogoutFilter -> JwtAuthenticationProcessingFilter -> CustomJsonUsernamePasswordAuthenticationFilter
-
 
         return http.build()
     }
