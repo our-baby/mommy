984c27ff8f3b89a5923a5e64521325bbb93fd84e
ybkim-dev <beanybigggy@gmail.com>
2024-07-11 00:05:27 +0900

    feat : 로그인 및 회원가입 로직 구현

--- src/main/kotlin/com/highschool/ourbaby/member/controller/MemberController.kt
+++ src/main/kotlin/com/highschool/ourbaby/member/controller/MemberController.kt
@@ -2,6 +2,7 @@ package com.highschool.ourbaby.member.controller
 
 import com.highschool.ourbaby.member.dto.MemberRequestDto
 import com.highschool.ourbaby.member.dto.MemberResponseDto
+import com.highschool.ourbaby.member.dto.SignInResponseDto
 import com.highschool.ourbaby.member.service.MemberService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,31 +10,36 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/members")
 class MemberController(
-	private val memberService: MemberService
+    private val memberService: MemberService,
 ) {
+    @PostMapping("sign-in")
+    fun getAuthenticationToken(@RequestHeader("certificated-token") certificatedToken: String) =
+        SignInResponseDto(memberService.getAuthenticationToken(certificatedToken))
 
-	@GetMapping
-	fun getAllMembers(): List<MemberResponseDto> = memberService.getAllMembers().map { MemberResponseDto(it) }
+    @GetMapping
+    fun getAllMembers(): List<MemberResponseDto> =
+        memberService.getAllMembers().map { MemberResponseDto(it) }
 
-	@GetMapping("/{id}")
-	fun getMemberById(@PathVariable id: Long) = MemberResponseDto(memberService.getMemberById(id))
+    @GetMapping("/{id}")
+    fun getMemberById(@PathVariable id: Long) = MemberResponseDto(memberService.getMemberById(id))
 
-	@PostMapping
-	fun createMember(@RequestBody memberRequestDto: MemberRequestDto) =
-		MemberResponseDto(memberService.createMember(memberRequestDto.toEntity()))
+    @PostMapping
+    fun createMember(@RequestBody memberRequestDto: MemberRequestDto) =
+        MemberResponseDto(memberService.createMember(memberRequestDto.toEntity()))
 
-	@PutMapping("/{id}")
-	fun updateMember(
-		@PathVariable id: Long,
-		@RequestBody memberRequestDto: MemberRequestDto
-	) = MemberResponseDto(memberService.updateMember(id, memberRequestDto.toEntity()))
+    @PutMapping("/{id}")
+    fun updateMember(
+        @PathVariable id: Long,
+        @RequestBody memberRequestDto: MemberRequestDto,
+    ) = MemberResponseDto(memberService.updateMember(id, memberRequestDto.toEntity()))
 
-	@DeleteMapping("/{id}")
-	fun deleteMember(@PathVariable id: Long) = memberService.deleteMember(id)
+    @DeleteMapping("/{id}")
+    fun deleteMember(@PathVariable id: Long) = memberService.deleteMember(id)
 }
--- src/main/kotlin/com/highschool/ourbaby/member/service/MemberService.kt
+++ src/main/kotlin/com/highschool/ourbaby/member/service/MemberService.kt
@@ -1,5 +1,8 @@
 package com.highschool.ourbaby.member.service
 
+import com.highschool.ourbaby.core.util.makeRandomString
+import com.highschool.ourbaby.member.domain.JoinType.NAVER
+import com.highschool.ourbaby.member.external.feign.oauth.NaverOAuthFeign
 import com.highschool.ourbaby.member.persistence.entity.MemberEntity
 import com.highschool.ourbaby.member.persistence.repository.MemberRepository
 import org.springframework.stereotype.Service
@@ -7,27 +10,48 @@ import kotlin.jvm.optionals.getOrElse
 
 @Service
 class MemberService(
-	private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val naverOAuthFeign: NaverOAuthFeign,
+    private val jwtService: JwtService,
 ) {
+    companion object {
+        private val NICKNAME_LENGTH = 30
+    }
 
-	fun getAllMembers(): List<MemberEntity> = memberRepository.findAll()
+    fun getAuthenticationToken(authCode: String): String {
+        val naverUser = naverOAuthFeign.getNaverUserInfo(authCode).toDomain()
 
-	fun getMemberById(id: Long): MemberEntity =
-		memberRepository.findById(id).getOrElse { throw NoSuchElementException("존재하지 않는 유저입니다.") }
+        val member = memberRepository.findByNameAndEmail(naverUser.name, naverUser.email)
+            ?: createMember(
+                MemberEntity(
+                    email = naverUser.email,
+                    name = naverUser.name,
+                    nickname = makeRandomString(NICKNAME_LENGTH),
+                    joinType = NAVER,
+                )
+            )
 
-	fun createMember(incoming: MemberEntity) = memberRepository.save(incoming)
+        return jwtService.createAccessToken(member.id)
+    }
 
-	fun updateMember(id: Long, incoming: MemberEntity): MemberEntity {
-		val member = getMemberById(id)
-		val update = MemberEntity(
-			id = member.id,
-			email = incoming.email,
-			name = incoming.name,
-			nickname = incoming.nickname,
-			joinType = incoming.joinType,
-		)
-		return memberRepository.save(update)
-	}
+    fun getAllMembers(): List<MemberEntity> = memberRepository.findAll()
 
-	fun deleteMember(id: Long) = memberRepository.deleteById(id)
+    fun getMemberById(id: Long): MemberEntity =
+        memberRepository.findById(id).getOrElse { throw NoSuchElementException("존재하지 않는 유저입니다.") }
+
+    fun createMember(incoming: MemberEntity) = memberRepository.save(incoming)
+
+    fun updateMember(id: Long, incoming: MemberEntity): MemberEntity {
+        val member = getMemberById(id)
+        val update = MemberEntity(
+            id = member.id,
+            email = incoming.email,
+            name = incoming.name,
+            nickname = incoming.nickname,
+            joinType = incoming.joinType,
+        )
+        return memberRepository.save(update)
+    }
+
+    fun deleteMember(id: Long) = memberRepository.deleteById(id)
 }
