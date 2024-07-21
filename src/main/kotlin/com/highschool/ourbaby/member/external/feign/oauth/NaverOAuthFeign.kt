3941f0669bbcee6f8218521e81dd2efe990564e0
mgyang53 <mg960503@gmail.com>
2024-07-21 16:56:28 +0900

    네이버 인증 api 헤더 Authorization 코드 수정

--- src/main/kotlin/com/highschool/ourbaby/member/external/feign/oauth/NaverOAuthFeign.kt
+++ src/main/kotlin/com/highschool/ourbaby/member/external/feign/oauth/NaverOAuthFeign.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = "NaverFeign", url = "\${feign.oauth.client.naver.user-info-url}")
 interface NaverOAuthFeign {
-    @GetMapping("v1/nid/me")
+    @GetMapping("v1/nid/me", consumes = ["application/xml"])
     fun getNaverUserInfo(
         @RequestHeader("Authorization") authCode: String,
     ): NaverResponse
