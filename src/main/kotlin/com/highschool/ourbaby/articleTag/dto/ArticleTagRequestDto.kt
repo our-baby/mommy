3063837608d46b00315b3960f920a28d179f3e22
nopro <rjwharry2003@gmail.com>
2024-07-21 17:31:04 +0900

    remove articleTag and unnecessary annotation

--- src/main/kotlin/com/highschool/ourbaby/articleTag/dto/ArticleTagRequestDto.kt
+++
@@ -1,6 +0,0 @@
-package com.highschool.ourbaby.articleTag.dto
-
-data class ArticleTagRequestDto(
-	val articleId: Long,
-	val tagId: Long,
-)
--- src/main/kotlin/com/highschool/ourbaby/core/exception/BadRequestException.kt
+++ src/main/kotlin/com/highschool/ourbaby/core/exception/BadRequestException.kt
@@ -1,8 +1,5 @@
 package com.highschool.ourbaby.core.exception
 
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.http.HttpStatus.BAD_REQUEST
 
-@ResponseStatus(BAD_REQUEST)
 class BadRequestException(message: String) : RuntimeException(message)
 
