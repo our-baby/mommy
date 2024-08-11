c3cec6e263d64f8949355e77b0fbc762cbcd5e84
ybkim-dev <beanybigggy@gmail.com>
2024-08-11 11:25:08 +0900

    style : 코드 정리

--- src/main/kotlin/com/highschool/ourbaby/core/config/JwtConfig.kt
+++ src/main/kotlin/com/highschool/ourbaby/core/config/JwtConfig.kt
@@ -7,16 +7,12 @@ import org.springframework.context.annotation.Configuration
 class JwtConfig(
     @Value("\${jwt.secretKey}")
     val secretKey: String,
-
     @Value("\${jwt.access.expiration}")
     val accessTokenExpirationPeriod: Long,
-
     @Value("\${jwt.refresh.expiration}")
     val refreshTokenExpirationPeriod: Long,
-
     @Value("\${jwt.access.header}")
     val accessHeader: String,
-
     @Value("\${jwt.refresh.header}")
     val refreshHeader: String,
 )
--- src/main/kotlin/com/highschool/ourbaby/member/service/MemberService.kt
+++ src/main/kotlin/com/highschool/ourbaby/member/service/MemberService.kt
@@ -2,10 +2,12 @@ package com.highschool.ourbaby.member.service
 
 import com.highschool.ourbaby.core.util.makeRandomString
 import com.highschool.ourbaby.member.domain.JoinType.NAVER
+import com.highschool.ourbaby.member.domain.oauth.Token
 import com.highschool.ourbaby.member.external.feign.oauth.NaverOAuthFeign
 import com.highschool.ourbaby.member.persistence.entity.MemberEntity
 import com.highschool.ourbaby.member.persistence.repository.MemberRepository
 import org.springframework.stereotype.Service
+import java.util.Date
 import kotlin.jvm.optionals.getOrElse
 
 @Service
@@ -18,40 +20,49 @@ class MemberService(
         private val NICKNAME_LENGTH = 30
     }
 
-    fun getAuthenticationToken(authCode: String): String {
-        val bearerPrefixed = "Bearer " + authCode
+    fun getAuthenticationToken(authCode: String): Token {
+        val naverUser =
+            "Bearer $authCode"
+                .let { naverOAuthFeign.getNaverUserInfo(it).toDomain() }
 
-        val naverUser = naverOAuthFeign.getNaverUserInfo(bearerPrefixed).toDomain()
-
-        val member = memberRepository.findByNameAndEmail(naverUser.name, naverUser.email)
-            ?: createMember(
-                MemberEntity(
-                    email = naverUser.email,
-                    name = naverUser.name,
-                    nickname = makeRandomString(NICKNAME_LENGTH),
-                    joinType = NAVER,
+        val member =
+            memberRepository.findByNameAndEmail(naverUser.name, naverUser.email)
+                ?: createMember(
+                    MemberEntity(
+                        email = naverUser.email,
+                        name = naverUser.name,
+                        nickname = makeRandomString(NICKNAME_LENGTH),
+                        joinType = NAVER,
+                    ),
                 )
-            )
 
-        return jwtService.createAccessToken(member.id)
+        val now = Date()
+
+        return Token(
+            accessToken = jwtService.createAccessToken(now, member.id),
+            refreshToken = jwtService.createRefreshToken(now, member.id),
+        )
     }
 
     fun getAllMembers(): List<MemberEntity> = memberRepository.findAll()
 
-    fun getMemberById(id: Long): MemberEntity =
-        memberRepository.findById(id).getOrElse { throw NoSuchElementException("존재하지 않는 유저입니다.") }
+    fun getMemberById(id: Long): MemberEntity = memberRepository.findById(id).getOrElse { throw NoSuchElementException("존재하지 않는 유저입니다.") }
 
     fun createMember(incoming: MemberEntity) = memberRepository.save(incoming)
 
-    fun updateMember(id: Long, incoming: MemberEntity): MemberEntity {
+    fun updateMember(
+        id: Long,
+        incoming: MemberEntity,
+    ): MemberEntity {
         val member = getMemberById(id)
-        val update = MemberEntity(
-            id = member.id,
-            email = incoming.email,
-            name = incoming.name,
-            nickname = incoming.nickname,
-            joinType = incoming.joinType,
-        )
+        val update =
+            MemberEntity(
+                id = member.id,
+                email = incoming.email,
+                name = incoming.name,
+                nickname = incoming.nickname,
+                joinType = incoming.joinType,
+            )
         return memberRepository.save(update)
     }
 
